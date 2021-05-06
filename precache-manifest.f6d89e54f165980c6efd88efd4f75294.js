5d747ec7552c64652edb5217548c63174eee49ed
amitrash <44778831+amitrash@users.noreply.github.com>
2021-05-06 13:38:39 +0300

    Updates

---
+++ precache-manifest.f6d89e54f165980c6efd88efd4f75294.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16eb0f68c87a2c76f7232f3993f43f97",
+    "url": "/website-react/index.html"
+  },
+  {
+    "revision": "a5733158102a13527cb7",
+    "url": "/website-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "697630ab24a3b84280f1",
+    "url": "/website-react/static/js/2.f0aec9ef.chunk.js"
+  },
+  {
+    "revision": "a5733158102a13527cb7",
+    "url": "/website-react/static/js/main.0bf8607c.chunk.js"
+  },
+  {
+    "revision": "3e91c1dd40d299726bf4",
+    "url": "/website-react/static/js/runtime-main.ec963f6d.js"
+  },
+  {
+    "revision": "eda4598198a00832fb334d5a4e588ead",
+    "url": "/website-react/static/media/01-thumbnail.eda45981.jpg"
+  },
+  {
+    "revision": "e52270b3af9b353d02078b01acd931ae",
+    "url": "/website-react/static/media/02-thumbnail.e52270b3.jpg"
+  },
+  {
+    "revision": "09c41872ba6c96c6efdcab7f202a71a7",
+    "url": "/website-react/static/media/03-thumbnail.09c41872.jpg"
+  },
+  {
+    "revision": "80c8ff5c4a1a0775616a5541086b1d51",
+    "url": "/website-react/static/media/04-thumbnail.80c8ff5c.jpg"
+  },
+  {
+    "revision": "68d8d9495f59fa5109f3b337d784cacc",
+    "url": "/website-react/static/media/05-thumbnail.68d8d949.jpg"
+  },
+  {
+    "revision": "20456fff3658b40a59a73ae9821a273f",
+    "url": "/website-react/static/media/06-thumbnail.20456fff.jpg"
+  },
+  {
+    "revision": "e25053c453155a9067f3cdc1bdbdd2a1",
+    "url": "/website-react/static/media/1.e25053c4.jpg"
+  },
+  {
+    "revision": "7006d6963a5fc645c84ff0e3e36e6a79",
+    "url": "/website-react/static/media/2.7006d696.jpg"
+  },
+  {
+    "revision": "838cdb868e76074f31c9a7776f9b92e5",
+    "url": "/website-react/static/media/3.838cdb86.jpg"
+  },
+  {
+    "revision": "458c7367d96405fd5c8867b42a2e85fb",
+    "url": "/website-react/static/media/Pepper2.458c7367.png"
+  },
+  {
+    "revision": "f5fad3dd08445344072bdbd2463b3d31",
+    "url": "/website-react/static/media/about.f5fad3dd.jpg"
+  }
+]);
